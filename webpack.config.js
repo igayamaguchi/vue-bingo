b0304b33e2ec59b063c7146c8703cbf71bba9eeb
yamaguchi1102 <m.yamaguchi1102@gmail.com>
2016-12-20 01:16:13 +0900

    webpackのコピープラグインとか導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,31 @@
 const webpack = require('webpack');
-const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+module.exports = {
+    entry: {
+        'jquery': ['jquery'],
+        'app': './src/bingo.js'
+    },
+    output: {
+        path: __dirname + '/bin/',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            { test: /\.vue$/, loader: 'vue' }
+        ]
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'jquery',
+            filename: 'lib/jquery.js',
+            minChunks: Infinity
+        }),
+        new CopyWebpackPlugin([
+            { from: './src/index.html' }
+        ])
+    ]
 
+};
